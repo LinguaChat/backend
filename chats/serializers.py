6650a0503b9b6adc9447acc724b7a8c5e966ada4
Marina <vorona.2003.bu@yandex.ru>
2023-07-05 17:35:41 +0300

    flake8 fix

--- chats/serializers.py
+++ chats/serializers.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-
 from rest_framework import serializers
 
 from .models import Chat
@@ -8,6 +7,6 @@ User = get_user_model()
 
 
 class ChatSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = Chat
