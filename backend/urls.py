bc9216882a72e9992ba17be9e143d8887264a10c
dimalright <yagam3333@gmail.com>
2023-07-09 21:09:17 +0300

    add url for media

--- backend/urls.py
+++ backend/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
@@ -12,3 +14,6 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
