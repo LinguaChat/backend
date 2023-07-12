7875dc2726db3117d1ccd3aaa454af37954d350e
Buzina Marina <77714654+BU-Marina@users.noreply.github.com>
2023-07-12 15:40:30 +0300

    code-style corrections 12.07

--- users/serializers.py
+++ users/serializers.py
@@ -78,16 +78,13 @@ class UserSerializer(DjoserSerializer,):
 
     def get_age(self, obj):
         """Вычисляем возраст пользователя."""
-
         if obj.birthdate:
             age_days = (dt.datetime.now().date() - obj.birthdate).days
             return int(age_days / 365)
-
         return None
 
     def create_foreign_languages(self, user, foreign_languages):
         """Создание объектов в промежуточной таблице."""
-
         for language in foreign_languages:
             UserLanguage.objects.create(
                 user=user,
@@ -98,24 +95,20 @@ class UserSerializer(DjoserSerializer,):
     def to_internal_value(self, data):
         """Кастомный метод to_internal_value, учитывающий
         наличие/отсутствие запроса на запись данных в through-таблицу."""
-
         if 'foreign_languages' in data:
             foreign_languages = data.pop('foreign_languages')
             result = super().to_internal_value(data)
             result['foreign_languages'] = foreign_languages
             return result
-
         return super().to_internal_value(data)
 
     def update(self, instance, validated_data):
         """Кастомные метод update, учитывающий
         наличие/отсутствие запроса на обновление through-таблицы."""
-
         if 'foreign_languages' in validated_data:
             UserLanguage.objects.filter(user=instance).delete()
             self.create_foreign_languages(
                 user=instance,
                 foreign_languages=validated_data.pop('foreign_languages')
             )
-
         return super().update(instance, validated_data)
