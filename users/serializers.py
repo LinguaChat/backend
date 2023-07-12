ef56c00d3173c56eeb1c2a3955af6f4d7ea7dc8c
Buzina Marina <77714654+BU-Marina@users.noreply.github.com>
2023-07-12 15:15:50 +0300

    code-style corrections 12.07

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,5 @@
+"""Сериализаторы для приложения users."""
+
 import datetime as dt
 
 from djoser.serializers import UserSerializer as DjoserSerializer
@@ -10,6 +12,7 @@ class CityNameField(serializers.RelatedField):
     """Кастомное поле, позволяющее делать update
     города по id и получать его строковое
     название при GET-запросе."""
+
     def to_representation(self, value):
         return value.name
 
@@ -21,6 +24,7 @@ class LanguageNameField(serializers.RelatedField):
     """Кастомное поле, позволяющее делать update
     языка по id и получать его строковое
     название при GET-запросе."""
+
     def to_representation(self, value):
         return value.name
 
@@ -30,6 +34,7 @@ class LanguageNameField(serializers.RelatedField):
 
 class UserLanguageSerializer(serializers.ModelSerializer):
     """Сериализатор для промежутоной модели Пользователь-Язык."""
+
     id = serializers.ReadOnlyField(source='language.id')
     language = serializers.ReadOnlyField(source='language.name')
 
@@ -44,6 +49,7 @@ class UserLanguageSerializer(serializers.ModelSerializer):
 
 class UserSerializer(DjoserSerializer,):
     """Сериализатор для модели пользователя."""
+
     age = serializers.SerializerMethodField()
     native_language = LanguageNameField(queryset=Language.objects.all())
     city = CityNameField(queryset=City.objects.all(), required=False)
@@ -72,13 +78,16 @@ class UserSerializer(DjoserSerializer,):
 
     def get_age(self, obj):
         """Вычисляем возраст пользователя."""
+
         if obj.birthdate:
             age_days = (dt.datetime.now().date() - obj.birthdate).days
             return int(age_days / 365)
+
         return None
 
     def create_foreign_languages(self, user, foreign_languages):
         """Создание объектов в промежуточной таблице."""
+
         for language in foreign_languages:
             UserLanguage.objects.create(
                 user=user,
@@ -89,6 +98,7 @@ class UserSerializer(DjoserSerializer,):
     def to_internal_value(self, data):
         """Кастомный метод to_internal_value, учитывающий
         наличие/отсутствие запроса на запись данных в through-таблицу."""
+
         if 'foreign_languages' in data:
             foreign_languages = data.pop('foreign_languages')
             result = super().to_internal_value(data)
@@ -100,10 +110,12 @@ class UserSerializer(DjoserSerializer,):
     def update(self, instance, validated_data):
         """Кастомные метод update, учитывающий
         наличие/отсутствие запроса на обновление through-таблицы."""
+
         if 'foreign_languages' in validated_data:
             UserLanguage.objects.filter(user=instance).delete()
             self.create_foreign_languages(
                 user=instance,
                 foreign_languages=validated_data.pop('foreign_languages')
             )
+
         return super().update(instance, validated_data)
