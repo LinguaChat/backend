b8756b4bc4379090dcf567814915d2f60f85d6a4
Marina <vorona.2003.bu@yandex.ru>
2023-07-12 15:28:31 +0300

    fixed urlpatterns

--- backend/urls.py
+++ backend/urls.py
@@ -13,9 +13,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(chat_router.urls)),
     path('api/v1/', include(user_router.urls)),
-    # Конфигурация Djoser для авторизации
-    path('api/v1/auth/', include('djoser.urls')),
-    path('api/v1/auth/', include('djoser.urls.jwt')),
     # Конфигурация DRF_Spectacular для просмотра документации
     path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/v1/schema/docs/',
