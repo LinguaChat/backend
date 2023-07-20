197ee9ff33ed30a01ea4d256685190cb450af241
Marina <vorona.2003.bu@yandex.ru>
2023-07-20 09:28:39 +0300

    restructuring

---
+++ backend/core/apps.py
@@ -0,0 +1,8 @@
+"""Файл конфигурации apps для приложения core."""
+
+from django.apps import AppConfig
+
+
+class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'core'
