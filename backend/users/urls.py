7b58269ab6262904cc11d7bc0fef2b82cd89c1bf
Marina <vorona.2003.bu@yandex.ru>
2023-07-21 21:17:51 +0300

    move chats, users urls

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -6,10 +6,11 @@ from rest_framework import routers
 
 from users.views import UserViewSet
 
-user_router = routers.DefaultRouter()
+router = routers.DefaultRouter()
 
-user_router.register('users', UserViewSet, basename='users')
+router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('auth/', include('djoser.urls.jwt')),
+    path('', include(router.urls)),
 ]
