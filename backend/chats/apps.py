62bf9867dd9121b6c0ac21700c4c7122b3d4f867
Marina <vorona.2003.bu@yandex.ru>
2023-07-21 12:21:02 +0300

    add verbose name for chats app

--- backend/chats/apps.py
+++ backend/chats/apps.py
@@ -6,3 +6,4 @@ from django.apps import AppConfig
 class ChatsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'chats'
+    verbose_name = 'Приложение для описания чатов и сообщений'
