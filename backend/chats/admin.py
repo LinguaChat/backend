c05c5a1a53112eac02e7be3164f850a048f79bd6
dimalright <yagam3333@gmail.com>
2023-07-24 22:12:56 +0300

    upd admin chat

--- backend/chats/admin.py
+++ backend/chats/admin.py
@@ -1,11 +1,22 @@
 """Административные настройки приложения chats."""
 
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
 from .models import Attachment, Chat, ChatMembers, Message, MessageReaders
 
-admin.site.register(Chat)
-admin.site.register(ChatMembers)
+User = get_user_model()
+
+
+class ChatMembersInline(admin.TabularInline):
+    model = ChatMembers
+
+
+class ChatAdmin(admin.ModelAdmin):
+    inlines = [ChatMembersInline]
+
+
+admin.site.register(Chat, ChatAdmin)
 admin.site.register(Attachment)
 admin.site.register(Message)
 admin.site.register(MessageReaders)
