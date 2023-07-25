a08501ef10af907813aa97f03de7550d0f069ae6
Marina <vorona.2003.bu@yandex.ru>
2023-07-25 16:37:28 +0300

    add data already exist check

--- backend/core/management/commands/importlanguages.py
+++ backend/core/management/commands/importlanguages.py
@@ -11,19 +11,23 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        cnt = 0
-        for isocode in LANG_INFO:
-            if len(isocode) == 2:  # we only care about the 2 letter iso codes
-                try:
-                    lang = Language(
-                        isocode=isocode,
-                        name=LANG_INFO[isocode]['name'],
-                        name_local=LANG_INFO[isocode]['name_local']
-                    )
-                    lang.save()
-                    cnt += 1
-                except Exception as e:
-                    raise CommandError(
-                        'Error adding language %s: %s' % (lang, e)
-                    )
-        self.stdout.write('Added %d languages to users' % cnt)
+        if not Language.objects.exists():
+            self.stdout.write('Languages not found, creating ones')
+            cnt = 0
+            for isocode in LANG_INFO:
+                if len(isocode) == 2:  # we only care about the 2 letter iso codes
+                    try:
+                        lang = Language(
+                            isocode=isocode,
+                            name=LANG_INFO[isocode]['name'],
+                            name_local=LANG_INFO[isocode]['name_local']
+                        )
+                        lang.save()
+                        cnt += 1
+                    except Exception as e:
+                        raise CommandError(
+                            'Error adding language %s: %s' % (lang, e)
+                        )
+            self.stdout.write('Added %d languages to users' % cnt)
+        else:
+            self.stdout.write('Languages were found, exiting')
