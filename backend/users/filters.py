96e0da3042256291f6bdda1b17ffb1c9029e0964
Marina <vorona.2003.bu@yandex.ru>
2023-07-29 09:00:12 +0300

    add users filters

--- backend/users/filters.py
+++ backend/users/filters.py
@@ -1,7 +1,11 @@
 """Фильтры приложения users."""
 
+from django.db.models import ExpressionWrapper, F, IntegerField
+from django.utils import timezone
+
 import django_filters as df
 
+from core.constants import LANGUAGE_SKILL_LEVELS
 from users.models import User
 
 
@@ -16,12 +20,38 @@ class CustomFilterList(df.Filter):
         return qs
 
 
+class AgeFilter(df.Filter):
+    """Фильтрация по возрасту."""
+
+    def filter(self, qs, value):
+        if value not in (None, ''):
+            qs = qs.annotate(
+                age=ExpressionWrapper(
+                    (timezone.now().date() - F('birth_date'))
+                    / 86400000000 / 365.25,
+                    output_field=IntegerField()
+                )
+            )
+            start, end = value.split(',')
+            return qs.filter(**{'%s__%s' %
+                                ('age', 'range'): (start, end)})
+        return qs
+
+
 class UserFilter(df.FilterSet):
     """Фильтр пользователей."""
 
-    age = df.NumberFilter(field_name='age')
+    age = AgeFilter()
+    country = CustomFilterList(
+        field_name='country__code', lookup_expr='in')
+    native_languages = CustomFilterList(
+        field_name='native_languages__isocode', lookup_expr='in')
     foreign_languages = CustomFilterList(
-        field_name='foreign_languages__name', lookup_expr='in')
+        field_name='foreign_languages__isocode', lookup_expr='in')
+    skill_level = df.ChoiceFilter(
+        choices=LANGUAGE_SKILL_LEVELS,
+        field_name='userforeignlanguage__skill_level'
+    )
 
     class Meta:
         model = User
@@ -29,5 +59,7 @@ class UserFilter(df.FilterSet):
             'age',
             'country',
             'gender',
+            'native_languages',
             'foreign_languages',
+            'skill_level',
         )
