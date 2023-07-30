cf7e6c70c64ec8f185cc5c4b1f5637ed607a6b83
dimalright <yagam3333@gmail.com>
2023-07-30 19:56:04 +0300

    change middleware

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'users.middleware.ActiveUserMiddleware',
+    'users.middleware.ActiveUserMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
