5ab015aee72601cae2ad5a1291396dbba4d1621a
dimalright <yagam3333@gmail.com>
2023-07-30 18:23:22 +0300

    fix w291

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -18,7 +18,7 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='localhost,127.0.0.1,').split
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'django.contrib.auth',  
+    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
