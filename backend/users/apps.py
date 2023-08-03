2188b460711017805cfdc2674b8d47fd3f6c4f83
Buzina Marina <77714654+BU-Marina@users.noreply.github.com>
2023-08-03 18:54:08 +0300

    fix: remove comment

--- backend/users/apps.py
+++ backend/users/apps.py
@@ -7,7 +7,3 @@ class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
     verbose_name = 'Приложение для описания пользователя'
-
-    # def ready(self):
-    #     # Импортируем модуль с определениями сигналов
-    #     import users.signals  # noqa
