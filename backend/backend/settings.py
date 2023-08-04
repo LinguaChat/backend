1049cdb6d8ed19b735b02019d5487383410ae514
Marina <vorona.2003.bu@yandex.ru>
2023-08-04 19:21:40 +0300

    update users create serializer

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -119,6 +119,7 @@ DJOSER = {
     },
     'SERIALIZERS': {
         'user': 'users.serializers.UserSerializer',
+        'user_create': 'users.serializers.UserSerializer',
         'current_user': 'users.serializers.UserSerializer',
     },
 }
