747019ad0a7397ca187df7631b38f9ca82363e0c
Marina <vorona.2003.bu@yandex.ru>
2023-08-24 09:15:55 +0300

    flake8 fix

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -2,10 +2,12 @@
 
 import os
 
-from channels.routing import URLRouter, ProtocolTypeRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from chats import routing  
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
+from chats import routing
 from chats.middleware import WebSocketJWTAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
